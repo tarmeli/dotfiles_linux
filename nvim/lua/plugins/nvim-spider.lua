14dbd7dfe566dbcb0b6b3bdc53fbbf0f980601b8
Tommi Tarkki <tommi.tarkki.ext@konecranes.com>
2025-09-07 09:18:02 +0300

    Initialize git repository

---
+++ nvim/lua/plugins/nvim-spider.lua
@@ -0,0 +1,12 @@
+return {
+  "chrisgrieser/nvim-spider",
+  lazy = true,
+  opts = {
+    keys = {
+      { "w", "<cmd>lua require('spider').motion('w')<CR>", mode = { "n", "o", "x" } },
+      { "e", "<cmd>lua require('spider').motion('e')<CR>", mode = { "n", "o", "x" } },
+      { "b", "<cmd>lua require('spider').motion('b')<CR>", mode = { "n", "o", "x" } },
+    },
+    subwordMovement = true,
+  },
+}
---
+++ nvim/lua/plugins/smear-cursor.lua
@@ -0,0 +1,20 @@
+return {}
+
+--return {
+--  "sphamba/smear-cursor.nvim",
+--
+--  opts = {
+--    -- Smear cursor when switching buffers or windows.
+--
+--    -- Smear cursor when moving within line or to neighbor lines.
+--    -- Use `min_horizontal_distance_smear` and `min_vertical_distance_smear` for finer control
+--
+--    -- Draw the smear in buffer space instead of screen space when scrolling
+--
+--    -- Set to `true` if your font supports legacy computing symbols (block unicode symbols).
+--    -- Smears will blend better on all backgrounds.
+--
+--    -- Smear cursor in insert mode.
+--    -- See also `vertical_bar_cursor_insert_mode` and `distance_stop_animating_vertical_bar`.
+--  },
+--}
